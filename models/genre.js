417b8226a0da641fc634c7e54a583f2d14a0848b
Arsalan <arsalancardoz04@gmail.com>
2023-08-14 11:06:42 +0530

    Fix genre URL virtual

--- models/genre.js
+++ models/genre.js
@@ -7,7 +7,7 @@ const GenreSchema = new Schema({
 })
 
 GenreSchema.virtual("url").get(function () {
-    return `/genre/${this._id}`
+    return `/catalog/genre/${this._id}`
 })
 
 module.exports = mongoose.model("Genre", GenreSchema)
